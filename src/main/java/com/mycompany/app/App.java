520dbbf186a63e19365fd431aac4376a717b7394
Jamie <jamie@openanswers.co.uk>
2017-07-11 16:50:11 +0100

    Changes to main file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,6 +1,8 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
 
 public final class App {
@@ -10,23 +12,18 @@ public final class App {
         do { //one iteration of the do/while loop equates to one day
 
             Thread.sleep(timeUntil(1, 0, 0));
-
             Thread.sleep(timeUntil(0, firstOfDay()[0], firstOfDay()[1]));
-
-            //function to return array of matches
-            Random r = new Random();
-            double T1 = r.nextGaussian() * 1.29 + 1.51;
-            r = new Random();
-            double T2 = r.nextGaussian() * 1.09 + 1.09;
-            int home = (int) Math.round(T1);
-            int away = (int) Math.round(T2);
-            if (T1 < 0) {
-                home = 0;
+            
+            for (String[] row : matchesToday()) {
+                produceImage(findScore(row));
             }
-            if (T2 < 0) {
-                away = 0;
-            }
-            System.out.println(home + " - " + away);
+            
+            tweet();
+            
+            Thread.sleep(timeUntil(0, 23, 55));
+            
+            summary();
+            
         } while (true);
     }
 
@@ -49,5 +46,48 @@ public final class App {
         hour--;
         int[] time = {hour, minute};
         return time;
+    }
+
+    public static List<String[]> matchesToday() {
+        List<String[]> rowList = new ArrayList<String[]>();
+
+        rowList.add(new String[]{"A", "B"});
+        rowList.add(new String[]{"C", "D"});
+        rowList.add(new String[]{"E", "F"});
+
+        return rowList;
+    }
+
+    public static void scores() {
+        Random r = new Random();
+        double T1 = r.nextGaussian() * 1.29 + 1.51;
+        r = new Random();
+        double T2 = r.nextGaussian() * 1.09 + 1.09;
+        int home = (int) Math.round(T1);
+        int away = (int) Math.round(T2);
+        if (T1 < 0) {
+            home = 0;
+        }
+        if (T2 < 0) {
+            away = 0;
+        }
+        System.out.println(home + " - " + away);
+    }
+    
+    public static void summary(){
+        //summary to date
+    }
+    
+    public static String[] findScore(String[] teams){
+        String[] score = {"1-0"};
+        return score;
+    }
+    
+    public static void produceImage(String[] results){
+        
+    }
+    
+    public static void tweet(){
+        
     }
 }
