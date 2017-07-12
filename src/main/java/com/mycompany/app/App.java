4181e0e0cef77959ff57a9b569a1eaada2eb0847
Jamie <jamie@openanswers.co.uk>
2017-07-12 16:02:31 +0100

    .

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,36 @@
 package com.mycompany.app;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 public final class App {
 
-    public static void main(String... args) throws InterruptedException {
+    public static void main(String... args) throws InterruptedException, ParseException {
+                
+        int[] odds = {581, 286, 182};
+        int[] oddInterval = new int[odds.length];
+        Random rand = new Random();
+        int oddsCount = 0;
+
+        for (int i = 0; i < odds.length; i++) {
+            oddsCount += odds[i];
+            oddInterval[i] = oddsCount;
+        }
+
+        int n = rand.nextInt(oddsCount);
+
+        for (int i = 0; i < odds.length; i++) {
+            if (n < oddInterval[i]) {
+                System.out.println("Outcome " + ++i);
+                break;
+            }
+        }
+
 
         do { //one iteration of the do/while loop equates to one day
 
@@ -40,10 +63,20 @@ public final class App {
         return c.getTimeInMillis() - System.currentTimeMillis();
     }
 
-    public static int[] firstOfDay() {
-        int hour = 15;
-        int minute = 0;
-        hour--;
+    public static int[] firstOfDay() throws ParseException, InterruptedException {
+        //go through JSON finding first with same day_of_month, month & year as today
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss");
+        String date = "2017-08-12T14:00:00Z";
+        Date dateSubstring = dateFormat.parse(date.substring(0, date.length() - 1));
+        int hour = 0;
+        
+        if(Calendar.getInstance().getTimeZone().useDaylightTime()){
+            hour = dateSubstring.getHours();
+        }
+        else{
+            hour = dateSubstring.getHours() - 1;
+        }
+        int minute = dateSubstring.getDate();
         int[] time = {hour, minute};
         return time;
     }
@@ -58,36 +91,20 @@ public final class App {
         return rowList;
     }
 
-    public static void scores() {
-        Random r = new Random();
-        double T1 = r.nextGaussian() * 1.29 + 1.51;
-        r = new Random();
-        double T2 = r.nextGaussian() * 1.09 + 1.09;
-        int home = (int) Math.round(T1);
-        int away = (int) Math.round(T2);
-        if (T1 < 0) {
-            home = 0;
-        }
-        if (T2 < 0) {
-            away = 0;
-        }
-        System.out.println(home + " - " + away);
-    }
-    
-    public static void summary(){
+    public static void summary() {
         //summary to date
     }
-    
-    public static String[] findScore(String[] teams){
+
+    public static String[] findScore(String[] teams) {
         String[] score = {"1-0"};
         return score;
     }
-    
-    public static void produceImage(String[] results){
-        
+
+    public static void produceImage(String[] results) {
+
     }
-    
-    public static void tweet(){
-        
+
+    public static void tweet() {
+
     }
 }
